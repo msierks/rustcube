8e2a374ce1f6a85497cb33a7502048fac45a5473
Michael Sierks <msierks117@gmail.com>
2016-07-08 13:15:20 -0500

    cleanup

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -9,32 +9,32 @@ pub struct Instruction(pub u32);
 impl Instruction {
     #[inline(always)]
     pub fn opcode(&self) -> u8 {
-        ((self.0  >> 26) & 0b11_1111) as u8
+        ((self.0  >> 26) & 0x3F) as u8
     }
 
     #[inline(always)]
     pub fn ext_opcode_a(&self) -> u8 {
-        ((self.0 >> 1) & 0b1_1111) as u8
+        ((self.0 >> 1) & 0x1F) as u8
     }
 
     #[inline(always)]
     pub fn ext_opcode_x(&self) -> u16 {
-        ((self.0 >> 1) & 0b11_1111_1111) as u16
+        ((self.0 >> 1) & 0x3FF) as u16
     }
 
     #[inline(always)]
     pub fn d(&self) -> usize {
-        ((self.0 >> 21) & 0b1_1111) as usize
+        ((self.0 >> 21) & 0x1F) as usize
     }
 
     #[inline(always)]
     pub fn a(&self) -> usize {
-        ((self.0 >> 16) & 0b1_1111) as usize
+        ((self.0 >> 16) & 0x1F) as usize
     }
 
     #[inline(always)]
     pub fn b(&self) -> usize {
-        ((self.0 >> 11) & 0b1_1111) as usize
+        ((self.0 >> 11) & 0x1F) as usize
     }
 
     #[inline(always)]
@@ -49,41 +49,43 @@ impl Instruction {
 
     #[inline(always)]
     pub fn crfd(&self) -> usize {
-        ((self.0 >> 23) & 0b111) as usize
+        ((self.0 >> 23) & 7) as usize
     }
 
+    // FixMe: this will always be false !!!
+    #[inline(always)]
     pub fn l(&self) -> bool {
         (self.0 & 0x200000) == 1
     }
 
     #[inline(always)]
     pub fn simm(&self) -> i16 {
-        (self.0 & 0b1111_1111_1111_1111) as i16
+        (self.0 & 0xFFFF) as i16
     }
 
     #[inline(always)]
     pub fn uimm(&self) -> u32 {
-        (self.0 & 0b1111_1111_1111_1111) as u32
+        (self.0 & 0xFFFF) as u32
     }
 
     #[inline(always)]
     pub fn li(&self) -> u32 {
-        ((self.0 >> 2) & 0b1111_1111_1111_1111_1111_1111)
+        ((self.0 >> 2) & 0xFFFFFF)
     }
 
     #[inline(always)]
     pub fn bo(&self) -> u8 {
-        ((self.0 >> 21) & 0b1_1111) as u8
+        ((self.0 >> 21) & 0x1F) as u8
     }
 
     #[inline(always)]
     pub fn bi(&self) -> usize {
-        ((self.0 >> 16) & 0b1_1111) as usize
+        ((self.0 >> 16) & 0x1F) as usize
     }
 
     #[inline(always)]
     pub fn bd(&self) -> u16 {
-        ((self.0 >> 2) & 0b1111_1111_1111_11) as u16
+        ((self.0 >> 2) & 0x3FFF) as u16
     }
 
     #[inline(always)]
@@ -98,37 +100,37 @@ impl Instruction {
 
     #[inline(always)]
     pub fn s(&self) -> usize {
-        ((self.0 >> 21) & 0b1_1111) as usize
+        ((self.0 >> 21) & 0x1F) as usize
     }
 
     #[inline(always)]
     pub fn sr(&self) -> usize {
-        ((self.0 >> 16) & 0b1111) as usize
+        ((self.0 >> 16) & 0xF) as usize
     }
 
     #[inline(always)]
     pub fn sh(&self) -> u8 {
-        ((self.0 >> 11) & 0b1_1111) as u8
+        ((self.0 >> 11) & 0x1F) as u8
     }
 
     #[inline(always)]
     pub fn mb(&self) -> u8 {
-        ((self.0 >> 6) & 0b1_1111) as u8
+        ((self.0 >> 6) & 0x1F) as u8
     }
 
     #[inline(always)]
     pub fn me(&self) -> u8 {
-        ((self.0 >> 1) & 0b1_1111) as u8
+        ((self.0 >> 1) & 0x1F) as u8
     }
 
     pub fn spr(&self) -> Spr {
-        let n = ((self.0 >> 6) & 0b111_1100_000) | ((self.0 >> 16) & 0b1_1111);
+        let n = ((self.0 >> 6) & 0x3E0) | ((self.0 >> 16) & 0x1F);
 
         Spr::from_u32(n).unwrap_or_else(|| Spr::UNKNOWN)
     }
 
     pub fn tbr(&self) -> Tbr {
-        let n = ((self.0 >> 6) & 0b111_1100_000) | ((self.0 >> 16) & 0b1_1111);
+        let n = ((self.0 >> 6) & 0x3E0) | ((self.0 >> 16) & 0x1F);
         Tbr::from_u32(n).unwrap_or_else(|| Tbr::UNKNOWN)
     }
 }
