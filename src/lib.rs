d99c68b84e8cc36c70a0f1c31d7d700fd26c6364
Michael Sierks <msierks117@gmail.com>
2016-09-12 18:15:59 -0500

    split bin and lib

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+extern crate byteorder;
+extern crate getopts;
+extern crate memmap;
+extern crate num;
+extern crate rustyline;
+
+#[macro_use]
+extern crate enum_primitive;
+
+mod audio_interface;
+mod command_processor;
+mod cpu;
+pub mod debugger;
+mod dsp_interface;
+mod dvd_interface;
+mod exi;
+pub mod gamecube;
+mod memory;
+mod memory_interface;
+mod pixel_engine;
+mod processor_interface;
+mod serial_interface;
+mod video_interface;
