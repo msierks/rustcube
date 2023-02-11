7346d3dfc55eed30313ffef2f2f546ea497676c7
Michael Sierks <msierks117@gmail.com>
2023-02-10 18:01:13 -0600

    Rewrite the cpu branch ops

--- src/cpu/cpu_branch.rs
+++ src/cpu/cpu_branch.rs
@@ -1,14 +1,15 @@
+const BO_DONT_DECREMENT: u8 = 0x4;
+
 fn op_bx(ctx: &mut Context, instr: Instruction) {
-    if instr.aa() == 1 {
-        ctx.cpu.nia = sign_ext_26(instr.li() << 2) as u32;
+    let address = sign_ext_26(instr.li() << 2) as u32;
+
+    if instr.aa() {
+        ctx.cpu.nia = address;
     } else {
-        ctx.cpu.nia = ctx
-            .cpu
-            .cia
-            .wrapping_add(sign_ext_26(instr.li() << 2) as u32);
+        ctx.cpu.nia = ctx.cpu.cia.wrapping_add(address);
     }
 
-    if instr.lk() != 0 {
+    if instr.lk() {
         ctx.cpu.spr[SPR_LR] = ctx.cpu.cia.wrapping_add(4);
     }
 
@@ -18,37 +19,23 @@ fn op_bx(ctx: &mut Context, instr: Instruction) {
 fn op_bcx(ctx: &mut Context, instr: Instruction) {
     let bo = instr.bo();
 
-    let ctr_ok = if bon(bo, 2) == 0 {
+    if bo & BO_DONT_DECREMENT == 0 {
         ctx.cpu.spr[SPR_CTR] = ctx.cpu.spr[SPR_CTR].wrapping_sub(1);
+    }
 
-        if bon(bo, 3) != 0 {
-            ctx.cpu.spr[SPR_CTR] == 0
-        } else {
-            ctx.cpu.spr[SPR_CTR] != 0
-        }
-    } else {
-        true
-    };
-
-    let cond_ok = if bon(bo, 0) == 0 {
-        bon(bo, 1) == ctx.cpu.cr.get_bit(instr.bi())
-    } else {
-        true
-    };
-
-    //println!("BO: {} ctr_ok: {} cond_ok: {}", bo, ctr_ok, cond_ok);
+    let ctr_ok = (bo >> 2) & 1 != 0 || (((ctx.cpu.spr[SPR_CTR] != 0) as u8 ^ (bo >> 1)) & 1) != 0;
+    let cond_ok = (bo >> 4) & 1 != 0 || (ctx.cpu.cr.get_bit(instr.bi()) == (bo >> 3) & 1);
 
     if ctr_ok && cond_ok {
-        if instr.aa() == 1 {
-            ctx.cpu.nia = sign_ext_16(instr.bd() << 2) as u32;
+        let address = sign_ext_16(instr.bd() << 2) as u32;
+
+        if instr.aa() {
+            ctx.cpu.nia = address;
         } else {
-            ctx.cpu.nia = ctx
-                .cpu
-                .cia
-                .wrapping_add(sign_ext_16(instr.bd() << 2) as u32);
+            ctx.cpu.nia = ctx.cpu.cia.wrapping_add(address);
         }
 
-        if instr.lk() != 0 {
+        if instr.lk() {
             ctx.cpu.spr[SPR_LR] = ctx.cpu.cia.wrapping_add(4);
         }
     }
@@ -59,17 +46,18 @@ fn op_bcx(ctx: &mut Context, instr: Instruction) {
 fn op_bcctrx(ctx: &mut Context, instr: Instruction) {
     let bo = instr.bo();
 
-    let cond_ok = if bon(bo, 0) == 0 {
-        ctx.cpu.cr.get_bit(instr.bi()) == bon(bo, 1)
-    } else {
-        true
-    };
+    // FIXME: check this logic
+    if bo & BO_DONT_DECREMENT == 0 {
+        panic!("bcctrx: Invalid instruction, BO[2] = 0");
+    }
 
-    if cond_ok {
-        ctx.cpu.nia = ctx.cpu.spr[SPR_CTR] & 0xFFFF_FFFC;
+    let cond_ok = ((bo >> 4) | (ctx.cpu.cr.get_bit(instr.bi()) == ((bo >> 3) & 1)) as u8) & 1;
 
-        if instr.lk() != 0 {
-            ctx.cpu.spr[SPR_LR] = ctx.cpu.cia + 4;
+    if cond_ok != 0 {
+        ctx.cpu.nia = ctx.cpu.spr[SPR_CTR] & (!3);
+
+        if instr.lk() {
+            ctx.cpu.spr[SPR_LR] = ctx.cpu.cia.wrapping_add(4);
         }
     }
 
@@ -79,28 +67,17 @@ fn op_bcctrx(ctx: &mut Context, instr: Instruction) {
 fn op_bclrx(ctx: &mut Context, instr: Instruction) {
     let bo = instr.bo();
 
-    let ctr_ok = if bon(bo, 2) == 0 {
+    if bo & BO_DONT_DECREMENT == 0 {
         ctx.cpu.spr[SPR_CTR] = ctx.cpu.spr[SPR_CTR].wrapping_sub(1);
+    }
 
-        if bon(bo, 3) != 0 {
-            ctx.cpu.spr[SPR_CTR] == 0
-        } else {
-            ctx.cpu.spr[SPR_CTR] != 0
-        }
-    } else {
-        true
-    };
+    let ctr_ok = ((bo >> 2) | ((ctx.cpu.spr[SPR_CTR] != 0) as u8 ^ (bo >> 1))) & 1;
+    let cond_ok = ((bo >> 4) | (ctx.cpu.cr.get_bit(instr.bi()) == ((bo >> 3) & 1)) as u8) & 1;
 
-    let cond_ok = if bon(bo, 0) == 0 {
-        bon(bo, 1) == ctx.cpu.cr.get_bit(instr.bi())
-    } else {
-        true
-    };
+    if ctr_ok != 0 && cond_ok != 0 {
+        ctx.cpu.nia = ctx.cpu.spr[SPR_LR] & (!3);
 
-    if ctr_ok && cond_ok {
-        ctx.cpu.nia = ctx.cpu.spr[SPR_LR] & 0xFFFF_FFFC;
-
-        if instr.lk() != 0 {
+        if instr.lk() {
             ctx.cpu.spr[SPR_LR] = ctx.cpu.cia.wrapping_add(4);
         }
     }
